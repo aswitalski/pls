7ac175174fe5046341054594da1b0a4bf185c0bc
Aleksander Świtalski <aleksander.switalski@gmail.com>
2025-12-28 22:01:31 +0100

    Abstract process exit

--- src/services/process.ts
+++ src/services/process.ts
@@ -1,6 +1,23 @@
 /**
+ * Process control abstraction for testability
+ * Allows mocking process.exit() in tests
+ */
+export interface ProcessControl {
+  exit(code: number): void;
+}
+
+export const defaultProcessControl: ProcessControl = {
+  exit: (code: number) => process.exit(code),
+};
+
+/**
  * Exit application after brief delay to allow UI to render
  */
-export function exitApp(code: 0 | 1) {
-  setTimeout(() => process.exit(code), 100);
+export function exitApp(
+  code: 0 | 1,
+  processControl: ProcessControl = defaultProcessControl
+) {
+  setTimeout(() => {
+    processControl.exit(code);
+  }, 100);
 }
